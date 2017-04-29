763c1f752b93e785d3892be153328615de4a834c
La Baik <labaik237@gmail.com>
2017-04-29 16:46:30 -0700

    install yelp package and load in env

--- config/index.js
+++ config/index.js
@@ -1,20 +1,6 @@
 'use strict'
+
 require('dotenv').config()
-
-const environment = process.env.NODE_ENV
-const port        = process.env.PORT || 8080
-const APPNAME     = process.env.APP_NAME || 'node_server'
-const jobs        = process.env.JOB_TYPES
-const mailer      = process.env.GMAIL
-const mailerPw    = process.env.GMAIL_PW
-const JWT_SECRET  = process.env.JWT_SECRET
-const CLIENT_URL  = process.env.CLIENT_URL
-const TWILIO = {
-  sid: process.env.TWILIO_ACCOUNT_SID,
-  token: process.env.TWILIO_AUTH_TOKEN,
-  number: process.env.TWILIO_NUM,
-}
-
 let log, db
 
 switch(environment) {
@@ -34,13 +20,22 @@ switch(environment) {
 module.exports = {
   db,
   log,
-  port,
-  APPNAME,
-  environment,
-  jobs,
-  mailer,
-  mailerPw,
-  JWT_SECRET,
-  CLIENT_URL,
-  TWILIO,
+  port: process.env.PORT || 8080,
+  APPNAME: process.env.APP_NAME || 'node_server',
+  environment: process.env.NODE_ENV,
+  jobs: process.env.JOB_TYPES,
+  mailer: process.env.GMAIL,
+  mailerPw: process.env.GMAIL_PW,
+  JWT_SECRET: process.env.JWT_SECRET,
+  CLIENT_URL: process.env.CLIENT_URL,
+  TWILIO: {
+    sid: process.env.TWILIO_ACCOUNT_SID,
+    token: process.env.TWILIO_AUTH_TOKEN,
+    number: process.env.TWILIO_NUM,
+  },
+  YELP: {
+    id: process.env.YELP_CLIENT_ID,
+    secret: process.env.YELP_CLIENT_SECRET
+  }
+
 }
