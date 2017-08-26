081e593ffc25b8b6b43b4e1d5e1908fdcabb57ec
La Baik <labaik237@gmail.com>
2017-08-26 15:01:01 -0700

    initial request to search yelp

--- controllers/ventures.js
+++ controllers/ventures.js
@@ -4,9 +4,16 @@ const { Venture } = require('../models')
 const yelp        = require('../libs/yelp')
 
 function create(req, res, next) {
+
   const { group, category, date, location } = req.body
   // search for pertinent venues on yelp
-  // find options
+  yelp.search({ date, location, category})
+    .then(result => {
+      req.data = { result }
+      next()
+    })
+    .catch(err => next(err))
+  // pick options
   // create venues
   // create venture with venue ids as options
   // return success message
--- libs/yelp/index.js
+++ libs/yelp/index.js
@@ -2,24 +2,60 @@
 
 const yelp      = require('yelp-fusion')
 const { YELP }  = require('../../config')
+const CONFIG = {
+  radius: 16000,
+  sort_by: 'rating',
+}
+
 let CLIENT
 
 /* gets yelp token and initializes yelp client */
-function initialize() {
+function initialize () {
+
+  console.log('Initializing Yelp Client')
+
   return yelp.accessToken(YELP.id, YELP.secret)
     .then(res => {
       const token = res.jsonBody.access_token
       CLIENT = yelp.client(token)
-      return token
+      return CLIENT
     })
     .catch(err => { throw err })
 }
 
-function search(params) {
-  if (!CLIENT) throw new Error('Yelp client not initialized')
-  return CLIENT.search(params)
+async function search (params) {
+
+  if (!CLIENT) {
+    CLIENT = await initialize()
+  }
+
+  return CLIENT.search(formatSearchParam(params))
     .then(res => res.jsonBody.businesses)
     .catch(err => { throw err })
 }
 
-module.exports { initialize, search }
+module.exports = { initialize, search }
+
+function formatSearchParam (params) {
+
+  const {
+    category,
+    date,
+    location: { zip, latitude, longitude },
+  } = params
+
+  return {
+    ...CONFIG,
+    open_at: toUnixDate(date),
+    categories: category,
+    location: zip,
+    latitude,
+    longitude,
+  }
+}
+
+function toUnixDate (dateString) {
+
+  const date = dateString || new Date()
+  return Date.parse(date)/1000
+}
